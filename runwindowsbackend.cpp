39aa2512ed6b834c963638506ec546e8818724d1
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2007-08-16 12:38:17 +0000

    registrymanager, importprogramwizard

--- runwindowsbackend.cpp
+++ runwindowsbackend.cpp
@@ -19,7 +19,12 @@
 #include <string.h>
 #include <wchar.h>
 #include <QDesktopServices>
+#include <QMessageBox>
 #include <QUrl>
+#include <QFile>
+#include "registrymanager.h"
+#include <QStringList>
+
 
 
 using namespace std;
@@ -53,47 +58,20 @@ void RunWindowsBackend::goTo(QString place)
 /**
  *	@brief Runs the command
  *	
- *	This doesn't need the full path as long as the command lies within $PATH
- *
  *	@param QString command
  *	The command - it will be passed to the system as-is
- *	@author Gigerl Martin / Goriup Phillip
+ *	@author Susanne Tschernegg
  */
 void RunWindowsBackend::run(QString command)
 {
-
-	//command = winword.exe
-
-    HKEY    hKey = NULL;
-    BYTE    bData[MAX_PATH + 1] = {0};
-    DWORD   dwDataLen = MAX_PATH;
-    string result;
-
-    QByteArray path("\\Applications\\"+command.toAscii()+"\\shell\\open\\command");
-    if (RegOpenKeyEx(HKEY_CLASSES_ROOT ,(WCHAR*) path.data(), 0,KEY_QUERY_VALUE,&hKey) != ERROR_SUCCESS) result = "0";
-    if (RegQueryValueEx(hKey,TEXT("App"),NULL,NULL,bData,&dwDataLen) == ERROR_SUCCESS) result = (string)((char*)bData);
-    else result = "0";
-    RegCloseKey(hKey);
-
-    Logger::log(path+result.c_str());
+   RegistryManager *rm = new RegistryManager();
+    rm->startProcess(command);
     
-    
-    
-	//QSettings settings(QSettings::UserScope, "Microsoft", "Windows");
-	//settings.beginGroup("CurrentVersion/Explorer/Shell Folders");
-	//Logger::log(settings.value("Personal").toString());
-
-	//QSettings registry;
-	//QString settingspath= "/Applications/"+command+"/shell/open/command";
-	//QString pathToCommand = registry.value(settingspath, "gibsnet" ).toString();
-	//Logger::log( "/Applications/"+command+"/shell/open/command: "+pathToCommand);
-	//QProcess::startDetached(pathToCommand);
-
-
-	/*char *asciicommand;
-	asciicommand = command.toAscii().data();
-	Logger::log(asciicommand);
-	system(asciicommand);*/
+    //just for testing
+    QStringList* programs = new QStringList();
+    programs = rm->getAllPrograms(".mp3");
+    //for(int i=0; i<programs->size(); i++)
+    //    QMessageBox::information(0, "progs", QString::number(i+1) + ": " + programs->at(i));
 }
 
 
