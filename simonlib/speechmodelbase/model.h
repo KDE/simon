8db7ab2458ff193570bc2fa6b43ceada20431d15
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2008-10-25 11:44:22 +0000

    Adding missing file...

---
+++ simonlib/speechmodelbase/model.h
@@ -0,0 +1,55 @@
+/*
+ *   Copyright (C) 2008 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#ifndef MODEL_H
+#define MODEL_H
+
+
+#include "speechmodelbase_export.h"
+
+#include <QString>
+#include <QHash>
+#include <QStringList>
+
+class SPEECHMODELBASE_EXPORT Model
+{
+	private:
+		int m_sampleRate, m_channels;
+		QByteArray m_hmmDefs;
+		QByteArray m_tiedList;
+		QByteArray m_dict;
+		QByteArray m_term;
+
+	public:
+		Model(int sampleRate, int channels, const QByteArray& hmmDefs,
+				const QByteArray& tiedList, const QByteArray& dict, const QByteArray& term);
+			       
+		~Model();
+		
+		int sampleRate() { return m_sampleRate; }
+		int channels() { return m_channels; }
+		QByteArray hmmDefs() { return m_hmmDefs; }
+		QByteArray tiedList() { return m_tiedList; }
+		QByteArray dict() { return m_dict; }
+		QByteArray term() { return m_term; }
+		
+};
+
+#endif
