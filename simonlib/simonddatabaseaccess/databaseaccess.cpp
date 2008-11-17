fed76c0474b675fd7707f7af2273c0157d677bc0
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2008-11-17 09:55:02 +0000

    Added missing  library...

---
+++ simonlib/simonddatabaseaccess/databaseaccess.cpp
@@ -0,0 +1,172 @@
+/*
+ *   Copyright (C) 2008 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "databaseaccess.h"
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QFile>
+#include <QVariant>
+
+#include <KDebug>
+#include <KStandardDirs>
+#include <KLocalizedString>
+
+DatabaseAccess::DatabaseAccess(QObject *parent) : QObject(parent)
+{
+	db=NULL;
+	userModel=NULL;
+}
+
+bool DatabaseAccess::init()
+{
+	if(db)
+	{
+		db->close();
+		delete db;
+		QSqlDatabase::removeDatabase("simond");
+	}
+
+	db = new QSqlDatabase(QSqlDatabase::addDatabase("QSQLITE", "simond"));
+	
+	QString databaseUrl=""; //TODO: get from settings
+
+	if (databaseUrl.isEmpty())
+	{
+		QString localKdeFile = KStandardDirs::locateLocal("data", "simond/simond.db");
+
+		if (!QFile::exists(localKdeFile))
+			QFile::copy(KStandardDirs::locate("data", "simond/simond.db"), 
+					localKdeFile);
+
+		databaseUrl = localKdeFile;
+	}
+	
+	db->setDatabaseName(databaseUrl);
+	m_database = databaseUrl;
+
+	
+	if (!db->open())	//open database
+	{
+		emit error(db->lastError().text());
+		
+		kDebug() << "Could not open db";
+		
+		return false;
+	}
+	
+	if (db->tables().isEmpty()) //create tables
+	{
+		kDebug() << "Creating tables";
+		return executeQuery("CREATE TABLE `User` ( "
+				    "UserID integer PRIMARY KEY AUTOINCREMENT, "
+				    "Name varchar(150) NOT NULL, "
+				    "Password varchar(250) not null"
+				    ");");
+	}
+	return true;
+}
+
+
+
+bool DatabaseAccess::executeQuery(const QString& query)
+{
+	if (!db) return false;
+
+	kDebug() << "Executing query " << query;
+	
+	QSqlQuery q(*db);
+	q.exec(query);
+	if (!q.isActive())
+	{
+		kDebug() << "Query failed";
+		emit error(db->lastError().text());
+		return false;
+	} else {
+		if(userModel) userModel->select();
+		return true;
+	}
+}
+
+void DatabaseAccess::closeConnection()
+{
+	db->close();
+}
+
+
+//USER
+bool DatabaseAccess::addUser(const QString& user, const QString& password)
+{
+	QString query = "INSERT INTO User (Name, Password) VALUES ('"+user+"', '"+password+"');";
+	return executeQuery(query);
+}
+
+bool DatabaseAccess::authenticateUser(const QString& user, const QString& password)
+{
+	QString cleanedPass = password;
+	cleanedPass = cleanedPass.replace("'", "\\'");
+	QString cleanedUser = user;
+	cleanedUser = cleanedUser.replace("'", "\\'");
+	QString query = "SELECT Name FROM User WHERE Name='"+cleanedUser+"' AND Password='"+cleanedPass+"'";
+	kDebug() << query;
+	QSqlQuery q(*db);
+
+	if (q.exec(query)) 
+	{
+		return q.first();
+	} else
+	{
+		emit error(db->lastError().text());
+		return false;
+	}
+}
+
+bool DatabaseAccess::setPassword(const QString& username, const QString& password)
+{
+	QString query = "UPDATE User SET Password='"+password+"' WHERE Name='"+username+"';";
+	return executeQuery(query);
+}
+
+
+bool DatabaseAccess::deleteUser(const QString& username)
+{
+	QString query = "DELETE FROM User WHERE Name='"+username+"';";
+	return executeQuery(query);
+}
+
+QSqlTableModel* DatabaseAccess::getUsers()
+{
+	if (userModel) return userModel;
+
+	QSqlTableModel *user= new QSqlTableModel(this, *db);
+	user->setTable("User");
+	user->removeColumn(0); //skip id
+	user->setHeaderData(0, Qt::Horizontal, i18n("Benutzername"));
+	user->setHeaderData(1, Qt::Horizontal, i18n("Verschlüsseltes Passwort"));
+	user->select();
+
+	this->userModel = user;
+
+	return user;
+}
+
+DatabaseAccess::~DatabaseAccess()
+{
+	db->close();
+	delete db;
+}
---
+++ simonlib/simonddatabaseaccess/databaseaccess.h
@@ -0,0 +1,60 @@
+/*
+ *   Copyright (C) 2008 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef DATABASEACCESS_H
+#define DATABASEACCESS_H
+
+#include "simonddatabaseaccess_export.h"
+#include <QObject>
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+
+class SIMONDDATABASEACCESS_EXPORT DatabaseAccess : public QObject
+{
+	Q_OBJECT
+
+	signals:
+		void error(const QString& error);
+
+	private:
+		QSqlDatabase *db;
+		QSqlTableModel *userModel;
+		QString m_database;
+		bool executeQuery(const QString& query);
+
+	public:
+		DatabaseAccess(QObject *parent=0);   
+		~DatabaseAccess();
+
+		bool init();
+		QString getDatabase() { return m_database; }
+		void closeConnection();
+
+		bool addUser(const QString& username, const QString& password);
+		bool deleteUser(const QString& username);
+		bool setPassword(const QString& username, const QString& password);
+
+		QSqlTableModel* getUsers();
+
+		bool authenticateUser(const QString& user, const QString& password);
+
+		bool isConnected() { return db->isOpen(); }
+};
+
+#endif
