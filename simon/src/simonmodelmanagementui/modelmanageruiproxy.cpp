e1daebcdc111d220915428219e8128851cc4598c
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2008-11-19 15:44:50 +0000

    Add missing files

---
+++ simon/src/simonmodelmanagementui/modelmanageruiproxy.cpp
@@ -0,0 +1,93 @@
+/*
+ *   Copyright (C) 2008 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "modelmanageruiproxy.h"
+#include <KMessageBox>
+#include <KDebug>
+#include <KLocalizedString>
+#include <speechmodelmanagement/trainingmanager.h>
+
+ModelManagerUiProxy::ModelManagerUiProxy(QObject *parent) : ModelManager(parent)
+{
+	connectedToServer=false;
+	connect (this, SIGNAL(modelChanged()), this, SLOT(slotModelChanged()));
+}
+
+
+void ModelManagerUiProxy::slotModelChanged()
+{
+	if (connectedToServer && KMessageBox::questionYesNo(0, i18n("Das Sprachmodell hat sich geändert.\n\nSoll es jetzt synchronisiert werden?")))
+	{
+		emit recompileModel();
+	}
+}
+
+void ModelManagerUiProxy::displayCompilationProtocol(const QString& protocol)
+{
+	kWarning() << protocol;
+	KMessageBox::detailedSorry(0, i18n("Modellprotokoll"), protocol);
+}
+
+void ModelManagerUiProxy::sampleNotAvailable(const QString& sample)
+{
+	if (KMessageBox::questionYesNo(0, i18n("Das Sprachsample \"%1\" konnte weder auf dem Server noch auf dem Computer gefunden werden.\n\nWollen Sie das Sample aus der Trainingsdatenbank entfernen?", sample)) == KMessageBox::Yes)
+	{
+		//kick some poor samples ass
+		startGroup();
+		QString sampleBaseName = sample.left(sample.length()-4);
+		kWarning() << sampleBaseName;
+		bool succ = TrainingManager::getInstance()->removeSample(sampleBaseName);
+		if (succ)
+			TrainingManager::getInstance()->savePrompts();
+
+		commitGroup(true /*silent*/);
+
+		if (!succ)
+			KMessageBox::error(0, i18n("Konnte das Sample nicht aus dem Trainingskorpus löschen"));
+		else
+			// and _do_ _it_ _again_
+			emit recompileModel();
+	}
+}
+
+
+
+
+void ModelManagerUiProxy::wordUndefined(const QString& word)
+{
+	if (KMessageBox::questionYesNoCancel(0, i18n("Beim Generieren des Sprachmodells wurde erkannt, dass das Wort \"%1\" in den Trainingsdaten vorkommt aber nicht phonetisch definiert ist.\n\nMöchten Sie das Wort jetzt hinzufügen?", word)) != KMessageBox::Yes)
+		return;
+
+
+}
+
+void ModelManagerUiProxy::classUndefined(const QString& undefClass)
+{
+}
+
+void ModelManagerUiProxy::phonemeUndefined(const QString& phoneme)
+{
+}
+
+
+ModelManagerUiProxy::~ModelManagerUiProxy()
+{
+
+}
+
---
+++ simon/src/simonmodelmanagementui/modelmanageruiproxy.h
@@ -0,0 +1,68 @@
+/*
+ *   Copyright (C) 2008 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#ifndef MODELMANAGERUIPROXY_H
+#define MODELMANAGERUIPROXY_H
+
+#include <speechmodelmanagement/modelmanager.h>
+#include "simonmodelmanagementui_export.h"
+
+/**
+ *	@class ModelManagementUiProxy
+ *	@brief Provides a "proxy" for the underlying modelmanager-concept-class which adds ui elements
+ *
+ *	@version 0.1
+ *	@date 18.11.2008
+ *	@author Peter Grasch
+*/
+
+class SIMONMODELMANAGEMENTUI_EXPORT ModelManagerUiProxy : public ModelManager
+{
+	Q_OBJECT
+
+signals:
+	void recompileModel();
+
+private slots:
+	void slotModelChanged();
+
+private:
+	bool connectedToServer;
+
+public:
+	ModelManagerUiProxy(QObject *parent=0);
+	~ModelManagerUiProxy();
+	
+	
+	void sampleNotAvailable(const QString&);
+	void wordUndefined(const QString&);
+	void classUndefined(const QString&);
+ 	void phonemeUndefined(const QString&);
+	void displayCompilationProtocol(const QString& protocol);
+
+	
+	void setConnectionStatus(bool connected)
+	{
+		connectedToServer = connected;
+	}
+};
+
+#endif
+
