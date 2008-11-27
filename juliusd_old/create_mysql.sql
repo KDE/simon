329e7289dc385117b091d1bbea6c0fc05bd3e927
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2008-11-27 20:52:54 +0000

    Committing fixes and partial translation

--- juliusd_old/create_mysql.sql
+++
@@ -1,9 +0,0 @@
--- drop database juliusd;
-create database juliusd;
-use juliusd;
-
-create table User (
-	UserId int primary key AUTO_INCREMENT,
-	Username varchar(50) not null,
-	Password varchar(250) not null
-);
