aaf2e1b82f4e058772a82572f9b0f45970f01f37
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2009-02-05 11:24:05 +0000

    Adding sudo-based install script

---
+++ build_ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script will build and install simon
+
+mkdir build 2> /dev/null
+cd build && cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` .. && make ; touch ./julius/gramtools/mkdfa/mkfa-1.44-flex/* && make && sudo make install && sudo ldconfig && kbuildsycoca4 && echo -e "**** Build completed ****\n\nThe executable file \"simon\" is now ready and has been installed.\n\nIssue \"simon\" to start it."
+
