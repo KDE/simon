7c5964b6ac8cb9bc9f4b2eea30d4296c332e7c2e
domar333 <domar333@a8ed545e-392b-0410-af97-f401dd8a169c>
2009-08-19 12:54:41 +0000

    Adding missing classes of calculator

---
+++ plugins/Commands/Calculator/token.cpp
@@ -0,0 +1,45 @@
+/*
+ *   Copyright (C) 2009 Dominik Neumeister <domar@gmx.at>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "token.h"
+
+Token::Token(char arOp, short priority)
+{
+    arOperator=arOp;
+    type=priority;
+}
+Token::Token(double num)
+{
+    number=num;
+    type=0;
+}
+short Token::getType()
+{
+    return type;
+}
+
+char Token::getArOperator()
+{
+    return arOperator;
+}
+
+double Token::getNumber()
+{
+    return number;
+}
---
+++ plugins/Commands/Calculator/token.h
@@ -0,0 +1,40 @@
+/*
+ *   Copyright (C) 2009 Dominik Neumeister <domar@gmx.at>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#ifndef TOKEN_H
+#define TOKEN_H
+
+class Token
+{
+    
+
+    public:
+	Token(char arOp, short type);
+        Token(double num);
+	char getArOperator();
+	double getNumber();	
+	short getType();
+
+    private:
+	char arOperator;
+	double number;
+	//@ type: 0=number; 1=+,-; 2=*,/
+	short type;
+};
+
+#endif
