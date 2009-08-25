c4bd1da618ee35d2f520e4fafc31b4a6a2e7d6d1
domar333 <domar333@a8ed545e-392b-0410-af97-f401dd8a169c>
2009-08-25 13:53:19 +0000

    as last commit ^^

---
+++ plugins/Commands/Keyboard/keyboardtab.h
@@ -0,0 +1,41 @@
+/*
+ *   Copyright (C) 2009 Mario Strametz <strmam06@htl-kaindorf.ac.at>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef KEYBOARDTAB_H
+#define KEYBOARDTAB_H
+
+#include "keyboardbutton.h"
+#include <QList>
+#include <QString>
+
+class KeyboardTab
+{
+	private:
+		QList<KeyboardButton *> buttonList;
+		QString tabName;
+		void addButton(KeyboardButton* b);
+		void delButton(int index);
+		void buttonLeft(int index);
+		void buttonRight(int index);
+		
+	public:
+		KeyboardTab(QString name);
+};
+
+#endif
