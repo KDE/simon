2c365dbbd8fa0d75339d736060a7f1bb22f0d2e4
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2009-10-06 20:35:49 +0000

    Committing missing scenariomanager

---
+++ simonlib/speechmodelmanagement/scenariomanager.cpp
@@ -0,0 +1,166 @@
+/*
+ *   Copyright (C) 2009 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#include "scenariomanager.h"
+
+#include <speechmodelmanagement/scenario.h>
+#include <speechmodelmanagement/shadowvocabulary.h>
+#include <simonscenariobase/scenariodisplay.h>
+
+#include <QFileInfo>
+#include <KDebug>
+#include <KGlobal>
+#include <KStandardDirs>
+
+ScenarioManager* ScenarioManager::instance;
+
+ScenarioManager::ScenarioManager(QObject *parent) : QObject(parent), currentScenario(0)
+{
+}
+
+bool ScenarioManager::init()
+{
+	bool succ = true;
+	if (!setupScenarios())
+		succ = false;
+
+	shadowVocab = ShadowVocabulary::createVocabulary();
+	if (!shadowVocab) return false;
+
+	connect(shadowVocab, SIGNAL(changed()), this, SLOT(shadowVocabularyChanged()));
+	return succ;
+}
+
+
+ShadowVocabulary* ScenarioManager::getShadowVocabulary()
+{
+	return shadowVocab;
+}
+
+// If force is true, every registered display will switch to this scenario
+// if not, only displays that already display the scenario will be updated
+void ScenarioManager::updateDisplays(Scenario* scenario, bool force)
+{
+	currentScenario = scenario;
+	foreach (ScenarioDisplay* display, scenarioDisplays) {
+		if (force || (display->currentScenario() ==  scenario)) {
+			display->displayScenario(scenario);
+		}
+	}
+}
+
+Scenario* ScenarioManager::getScenario(const QString& id)
+{
+	foreach (Scenario *scenario, scenarios) {
+		if (scenario->id() == id)
+			return scenario;
+	}
+	return NULL;
+}
+
+bool ScenarioManager::setupScenarios()
+{
+	bool success = true;
+
+	kDebug() << "Setting up scenarios...";
+	//Currently loads all scenarios
+	//Should be replaced with "only selected ones" through the manage scenario action
+	QStringList scenarioSrcs = KGlobal::dirs()->findAllResources("appdata", "scenarios/");
+	QStringList scenarioIds;
+
+	foreach (const QString& src, scenarioSrcs) {
+		QFileInfo f(src);
+		QString idToBe = f.fileName();
+		if (!scenarioIds.contains(idToBe)) 
+			scenarioIds << idToBe;
+	}
+
+	kDebug() << "Found scenarios: " << scenarioIds;
+
+	foreach (const QString& id, scenarioIds) {
+		Scenario *s = new Scenario(id);
+		kDebug() << "Initializing scenario" << id;
+		if (!s->init()) {
+//			KMessageBox::error(0, i18n("Failed to initialize scenario \"%1\"").arg(id));
+			success = false;
+		} else {
+			connect(s, SIGNAL(changed(Scenario*)), this, SLOT(updateDisplays(Scenario*)));
+			connect(s, SIGNAL(changed(Scenario*)), this, SLOT(scenariosChanged()));
+			scenarios << s;
+		}
+	}
+	return success;
+}
+
+QStringList ScenarioManager::getTerminals(SpeechModel::ModelElements elements)
+{
+	/*ShadowVocabulary=1,
+	ScenarioVocabulary=2,
+	ScenarioGrammar=4,
+	ScenarioActions=8*/
+	QStringList terminals;
+
+	terminals << getCurrentScenario()->getTerminals(elements);
+
+	if (elements & SpeechModel::ShadowVocabulary) {
+		QStringList shadowTerminals = shadowVocab->getTerminals();
+		foreach (const QString& terminal, shadowTerminals)
+			if (!terminals.contains(terminal))
+				terminals << terminal;
+	}
+
+	return terminals;
+}
+
+bool ScenarioManager::renameTerminal(const QString& terminal, const QString& newName, SpeechModel::ModelElements affect)
+{
+	bool success = true;
+
+	if (affect & SpeechModel::ShadowVocabulary) {
+		if (!(shadowVocab->renameTerminal(terminal, newName)))
+			success=false;
+	}
+
+	if (!getCurrentScenario()->renameTerminal(terminal, newName, affect))
+		success = false;
+
+	return success;
+}
+
+QList<Word*>* ScenarioManager::findWords(const QString& name, SpeechModel::ModelElements elements)
+{
+	QList<Word*>* words = new QList<Word*>();
+	if (elements & SpeechModel::ShadowVocabulary) {
+		QList<Word*>* newWords = shadowVocab->findWords(name);
+		if (newWords) words->append(*newWords);
+	}
+
+	if (elements & SpeechModel::ScenarioVocabulary) {
+		QList<Word*>* newWords = getCurrentScenario()->findWords(name);
+		if (newWords) words->append(*newWords);
+	}
+
+	return words;
+}
+
+ScenarioManager::~ScenarioManager()
+{
+}
+
---
+++ simonlib/speechmodelmanagement/scenariomanager.h
@@ -0,0 +1,88 @@
+/*
+ *   Copyright (C) 2009 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+
+#ifndef SIMON_SCENARIOMANAGER_H
+#define SIMON_SCENARIOMANAGER_H
+
+/**
+ *	@class ScenarioManager
+ *	@author Peter Grasch
+*/
+
+#include <QObject>
+#include <QStringList>
+#include <QList>
+#include "speechmodel.h"
+#include "simonmodelmanagement_export.h"
+
+class Word;
+class Scenario;
+class ScenarioDisplay;
+class ShadowVocabulary;
+
+class MODELMANAGEMENT_EXPORT ScenarioManager : public QObject {
+Q_OBJECT
+
+signals:
+	void scenariosChanged();
+	void shadowVocabularyChanged();
+
+public:
+	static ScenarioManager *getInstance() {
+		if (!instance) instance = new ScenarioManager();
+		return instance;
+	}
+	
+	ScenarioManager(QObject *parent=0);
+	~ScenarioManager();
+
+	QList<Scenario*> getScenarios() { return scenarios; }
+	void registerScenarioDisplay(ScenarioDisplay *display) {
+		scenarioDisplays.append(display);
+	}
+	Scenario *getScenario(const QString& id);
+	Scenario *getCurrentScenario() { return currentScenario; }
+	bool init();
+	ShadowVocabulary* getShadowVocabulary();
+
+	QStringList getTerminals(SpeechModel::ModelElements elements);
+	bool renameTerminal(const QString& terminal, const QString& newName, SpeechModel::ModelElements affect);
+
+	QList<Word*>* findWords(const QString& name, SpeechModel::ModelElements elements);
+
+	
+public slots:
+	// If force is true, every registered display will switch to this scenario
+	// if not, only displays that already display the scenario will be updated
+	void updateDisplays(Scenario* scenario, bool force=false);
+
+private:
+	static ScenarioManager *instance;
+	ShadowVocabulary *shadowVocab;
+	Scenario *currentScenario;
+	
+	QList<Scenario*> scenarios;
+	QList<ScenarioDisplay*> scenarioDisplays;
+
+	bool setupScenarios();
+};
+
+#endif
+
