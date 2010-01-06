660e9935612b199998ff071ab616a085a5106033
speechtotext <speechtotext@a8ed545e-392b-0410-af97-f401dd8a169c>
2010-01-06 10:58:14 +0000

    Adding missing file

---
+++ simonlib/eventsimulation/clickmode.h
@@ -0,0 +1,35 @@
+/*
+ *   Copyright (C) 2010 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SIMON_CLICKMODE_H_A54B4D68778B4988A4AB1950123164C2
+#define SIMON_CLICKMODE_H_A54B4D68778B4988A4AB1950123164C2
+
+namespace EventSimulation {
+	enum ClickMode  {
+		LMB=1,
+		LMBDouble=2,
+		LMBDown=3,
+		LMBUp=4,
+		RMB=5,
+		MMB=6
+	};
+}
+
+#endif
+
