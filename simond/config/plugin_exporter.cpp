23542c813eddf9bd14b729a74e6654c89b65e419
Peter Grasch <grasch@simon-listens.org>
2010-02-10 09:25:48 +0100

    Introducing multikcmproxy to work around a bug in kde on windows; Porting simond config to multikcmproxy (WIP)

--- simond/config/plugin_exporter.cpp
+++ simond/config/plugin_exporter.cpp
@@ -4,10 +4,24 @@
 #include "simonduserconfiguration.h"
 #include "simondnetworkconfiguration.h"
 #include <kgenericfactory.h>
+#include <KLocalizedString>
+#include <simonuicomponents/multikcmview.h>
+
+class SimondConfigMultiKCMView : public MultiKCMView
+{
+public:
+	SimondConfigMultiKCMView(QWidget* parent, const QVariantList& args=QVariantList()) :
+		MultiKCMView(parent, args)
+	{
+		registerModule(new SimondUserConfiguration(parent, args), KIcon("user-properties"), i18n("User"));
+		registerModule(new SimondNetworkConfiguration(parent, args), KIcon("network-disconnect"), i18n("Network"));
+	}
+};
 
 K_PLUGIN_FACTORY( SimondSettingsFactory, 
-			registerPlugin< SimondUserConfiguration >("SimondUserConfiguration"); 
-			registerPlugin< SimondNetworkConfiguration >("SimondNetworkConfiguration");
+			registerPlugin< SimondConfigMultiKCMView >();
+			//registerPlugin< SimondUserConfiguration >("SimondUserConfiguration"); 
+			//registerPlugin< SimondNetworkConfiguration >("SimondNetworkConfiguration");
 		)
 
 K_EXPORT_PLUGIN( SimondSettingsFactory("simond"); )
