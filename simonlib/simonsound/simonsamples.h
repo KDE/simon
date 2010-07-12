2346600d9b06b173fb08039c39f3247300463273
Peter Grasch <grasch@simon-listens.org>
2010-07-12 10:00:35 +0300

    Adding missing file

---
+++ simonlib/simonsound/simonsamples.h
@@ -0,0 +1,36 @@
+/*
+ *   Copyright (C) 2010 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SIMON_SIMONSAMPLES_H_33F50DCA0C3D401FADDFBFD80B4E16F4
+#define SIMON_SIMONSAMPLES_H_33F50DCA0C3D401FADDFBFD80B4E16F4
+
+namespace SimonSamples
+{
+  enum SampleProblem
+  {
+    None=0,
+    Clipping=1,
+    SNRTooLow=2
+  };
+  Q_DECLARE_FLAGS(SampleProblems, SampleProblem);
+}
+
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(SimonSamples::SampleProblems);
+#endif
