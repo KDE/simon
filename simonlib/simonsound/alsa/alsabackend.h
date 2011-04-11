88474d457b484c1af05cc6157386257db6d44d11
Peter Grasch <grasch@simon-listens.org>
2011-04-11 12:17:35 +0200

    Adding soundbackends

---
+++ simonlib/simonsound/alsa/alsabackend.h
@@ -0,0 +1,77 @@
+/*
+ *   Copyright (C) 2011 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SIMON_ALSABACKEND_H_BAC60651BE6A419EA6156220815A2AAD
+#define SIMON_ALSABACKEND_H_BAC60651BE6A419EA6156220815A2AAD
+
+#include <simonsound/simonsound.h>
+#include <simonsound/soundbackend.h>
+#include <alsa/asoundlib.h>
+#include <QStringList>
+
+class ALSALoop;
+class ALSACaptureLoop;
+class ALSAPlaybackLoop;
+
+class ALSABackend : public SoundBackend
+{
+  friend class ALSAPlaybackLoop;
+  friend class ALSACaptureLoop;
+
+  private:
+    snd_pcm_t *m_handle;
+    ALSALoop *m_loop;
+
+    int m_bufferSize;
+    int m_periodSize;
+    unsigned int m_chunks;
+    QStringList getDevices(SimonSound::SoundDeviceType type);
+
+    snd_pcm_t* openDevice(SimonSound::SoundDeviceType type, 
+        const QString& device, int channels, int samplerate);
+
+  protected:
+    void errorRecoveryFailed();
+    bool stop();
+
+  public:
+    ALSABackend();
+    ~ALSABackend();
+
+    QStringList getAvailableInputDevices();
+    QStringList getAvailableOutputDevices();
+    bool check(SimonSound::SoundDeviceType type, const QString& device, int channels, int samplerate);
+
+    QString getDefaultInputDevice();
+    QString getDefaultOutputDevice();
+
+    bool prepareRecording(const QString& device, int& channels, int& samplerate);
+    bool startRecording(SoundBackendClient *client);
+    bool stopRecording();
+
+    bool preparePlayback(const QString& device, int& channels, int& samplerate);
+    bool startPlayback(SoundBackendClient *client);
+    bool stopPlayback();
+
+    int bufferSize();
+};
+
+#endif
+
+
---
+++ simonlib/simonsound/soundbackendclient.h
@@ -0,0 +1,32 @@
+/*
+ *   Copyright (C) 2011 Peter Grasch <grasch@simon-listens.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef SIMON_SOUNDBACKENDCLIENT_H_BAC60651BE6A419EA6156220815A2AAD
+#define SIMON_SOUNDBACKENDCLIENT_H_BAC60651BE6A419EA6156220815A2AAD
+
+class SoundBackendClient
+{
+  public:
+    virtual qint64 readData(char *, qint64) { return -1; }
+    virtual qint64 writeData(const char *, qint64) { return -1; }
+    virtual ~SoundBackendClient() {};
+};
+
+#endif
+
