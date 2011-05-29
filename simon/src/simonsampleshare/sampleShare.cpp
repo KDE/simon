3ab0d923dcf6c3035bfe54e0705f66e0aa249a37
Alessandro Buggin <alessandrobuggin@gmail.com>
2011-05-29 16:44:54 +0200

    added support on toolbar

--- simon/src/simonsampleshare/sampleShare.cpp
+++ simon/src/simonsampleshare/sampleShare.cpp
@@ -1,11 +1,11 @@
 /*
  * create a new library next to simonappcore, simonactionsui, etc. called "simonsampleshare";
  * start by creating a ui (KDialog), that simply says "Hello World";
- * integrate this library and dialog into simon by adding a menu option to "Actions" called "Publish samples" displaying the
- * dialog from the new library
- * 
+ * integrate this library and dialog into simon by adding 
+ * a menu option to "Actions" called "Publish samples" 
+ * displaying the dialog from the new library
  */
-//
+
 #include "sampleShare.h"
 sampleShare::sampleShare(QWidget* parent) : KXmlGuiWindow(parent){
   
