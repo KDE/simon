74559c7e8ec86d944c5f213689272224ad7eed5f
Adam Nash <adam.t.nash@gmail.com>
2011-07-18 13:30:58 -0400

    >Made model cache settings preserved across multiple sessions of simon with cache data files

--- simonlib/simoncontextadapter/contextadapter.cpp
+++ simonlib/simoncontextadapter/contextadapter.cpp
@@ -2,6 +2,7 @@
 #include <KDebug>
 #include <QDir>
 #include <KStandardDirs>
+#include <QTextStream>
 
 ContextAdapter::ContextAdapter(QString username, QObject *parent) :
     QObject(parent)
@@ -10,8 +11,6 @@ ContextAdapter::ContextAdapter(QString username, QObject *parent) :
     m_modelCompilationAdapter = new ModelCompilationAdapterHTK(username, this);
     m_username = username;
     m_deactivatedScenarios = QStringList();
-
-    //TODO:make a file in the cache folder that will save the currentScenarioSet
     m_currentScenarioSet = QStringList();
 
     connect(m_modelCompilationManager, SIGNAL(modelCompiled()),
@@ -43,14 +42,38 @@ ContextAdapter::ContextAdapter(QString username, QObject *parent) :
     connect(m_modelCompilationManager, SIGNAL(activeModelCompilationAborted()),
             this, SIGNAL(activeModelCompilationAborted()));
 
+    //make sure that the cache directory and cache info files exist
     QString cacheDir = KStandardDirs::locateLocal("appdata", "models/"+m_username+"/cached/");
     QDir dir;
     if (!dir.exists(cacheDir))
     {
         kDebug() << "making cache directory";
         dir.mkpath(cacheDir);
+    }     
+
+    //load the base scenario list
+    QFile scenariosFile(cacheDir + "ScenarioList");
+    if (scenariosFile.exists())
+    {
+        scenariosFile.open(QFile::ReadOnly);
+        QDataStream fileStream(&scenariosFile);
+        fileStream >> m_currentScenarioSet;
+        scenariosFile.close();
+
+        kDebug() << "Loaded base scenario list: " << m_currentScenarioSet;
     }
 
+    //load the model cache lookup hash table
+    QFile lookupFile(cacheDir + "CacheLookup");
+    if (lookupFile.exists())
+    {
+        lookupFile.open(QFile::ReadOnly);
+        QDataStream lookupStream(&lookupFile);
+        lookupStream >> m_modelCache;
+        lookupFile.close();
+
+        kDebug() << "Loaded model cache lookup: " << m_modelCache;
+    }
 }
 
 ContextAdapter::~ContextAdapter()
@@ -100,6 +123,14 @@ void ContextAdapter::storeModelInCache()
     file.copy(activeDir+"hmmdefs_loc", cachedModelDir+"hmmdefs_loc");
     file.copy(activeDir+"julius.log", cachedModelDir+"julius.log");
     file.copy(activeDir+"julius.jconf", cachedModelDir+"julius.jconf");
+
+    //save model cache lookup
+    QFile lookupFile(cacheDir + "CacheLookup");
+    lookupFile.open(QFile::WriteOnly | QFile::Truncate);
+    QDataStream lookupStream(&lookupFile);
+    lookupStream << m_modelCache;
+    lookupFile.close();
+    kDebug() << "Model cache lookup saved: " << m_modelCache;
 }
 
 void ContextAdapter::clearCache()
@@ -108,12 +139,23 @@ void ContextAdapter::clearCache()
     QFile file;
     QStringList cacheDirs;
     QStringList cachedFiles;
+    QString cacheDir = KStandardDirs::locateLocal("appdata", "models/"+m_username+"/cached/");
 
     kDebug() << "Clearing model cache";
 
     m_modelCache.clear();
 
-    QString cacheDir = KStandardDirs::locateLocal("appdata", "models/"+m_username+"/cached/");
+    QFile lookupFile(cacheDir + "CacheLookup");
+    if (lookupFile.exists())
+    {
+        lookupFile.open(QFile::Truncate);
+        QDataStream lookupStream(&lookupFile);
+        lookupStream >> m_modelCache;
+        lookupFile.close();
+
+        kDebug() << "Model cache lookup refreshed";
+    }
+
     QDir initialDir = dir.current();
     dir.setCurrent(cacheDir);
 
@@ -121,11 +163,9 @@ void ContextAdapter::clearCache()
     foreach (QString dirInfo, cacheDirs)
     {
         dir.setCurrent(dirInfo);
-        //kDebug() << "Clearing out directory " + dir.absoluteFilePath(dir.dirName());
         cachedFiles = dir.entryList(QDir::Files);
         foreach (QString fileInfo, cachedFiles)
         {
-            //kDebug() << "Deleting file " + fileInfo;
             file.remove(fileInfo);
         }
         dir.setCurrent(cacheDir);
@@ -146,12 +186,25 @@ bool ContextAdapter::startAdaption(ModelCompilationAdapter::AdaptionType adaptio
     QString cachedModelDir;
     QFile file;
 
-    //check to see if the scenarios are different (in which case the cache would need to be cleared)
+    //check to see if the base scenarios are different (in which case the cache would need to be cleared)
     if (m_currentScenarioSet.join(",") != scenarioPathsIn.join(","))
     {
         kDebug() << "There is a new base scenario list!";
         clearCache();
+
         m_currentScenarioSet = scenarioPathsIn;
+
+        //save the new base scenario list
+        QFile scenariosFile(cacheDir + "ScenarioList");
+        scenariosFile.open(QFile::Truncate | QFile::WriteOnly);
+        QDataStream fileStream(&scenariosFile);
+        fileStream << scenarioPathsIn;
+        scenariosFile.close();
+         kDebug() << "Saved new base scenario list: " << m_currentScenarioSet;
+    }
+    else
+    {
+        kDebug() << "Still using base scenario list: " << m_currentScenarioSet;
     }
 
     //get the list of NOT deactivated scenario paths
