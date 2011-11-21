KDE/simon

commit cd255d2481734665532eea2fd08bf6dda9d07178

More QList as pointer.

simon/src/simonappcore/simoncontrol.cpp
@@ -203,13 +203,12 @@ void SimonControl::disconnectFromServer()
  *
  *	@author Peter Grasch
  */
-void SimonControl::wordRecognised(RecognitionResultList* recognitionResults)
+void SimonControl::wordRecognised(RecognitionResultList recognitionResults)
 {
   if (status != SimonControl::ConnectedActivated) return;
 
   kDebug() << "Received recognition results...";
   ActionManager::getInstance()->processRawResults(recognitionResults);
-  delete recognitionResults;
 }
 
 
@@ -250,10 +249,10 @@ void SimonControl::recognitionStatusChanged(RecognitionControl::RecognitionStatu
 }
 
 
-void SimonControl::setStatus(SimonControl::SystemStatus status)
+void SimonControl::setStatus(SimonControl::SystemStatus newStatus)
 {
-  this->status = status;
-  emit systemStatusChanged(status);
+  status = newStatus;
+  emit systemStatusChanged(newStatus);
 }
 
 

simonlib/simonactions/actionmanager.cpp
@@ -50,8 +50,6 @@
 ActionManager* ActionManager::instance;
 
 ActionManager::ActionManager(QObject* parent) : QObject(parent),
-currentlyPromptedListOfResults(0),
-greedyReceivers(new QList<GreedyReceiver*>()),
 minimumConfidenceThreshold(0.45),
 useDYM(false)
 {
@@ -75,13 +73,13 @@ ActionManager* ActionManager::getInstance()
 
 void ActionManager::registerGreedyReceiver(GreedyReceiver *receiver)
 {
-  greedyReceivers->insert(0, receiver);
+  greedyReceivers.insert(0, receiver);
 }
 
 
 void ActionManager::deRegisterGreedyReceiver(GreedyReceiver *receiver)
 {
-  greedyReceivers->removeAll(receiver);
+  greedyReceivers.removeAll(receiver);
 }
 
 
@@ -96,18 +94,17 @@ void ActionManager::retrieveRecognitionResultFilteringParameters()
 
 bool ActionManager::triggerCommand(const QString& type, const QString& trigger, bool silent)
 {
-  if (type == "simonrecognitionresult" && currentlyPromptedListOfResults) {
+  if (type == "simonrecognitionresult") {
     //result from a did-you-mean popup
     QString selectedSentence = trigger;
     selectedSentence.remove(QRegExp("^[0-9][0-9]?[0-9]?%: "));
-    for (int i=0; i< currentlyPromptedListOfResults->count(); i++) {
-      QString sentence = currentlyPromptedListOfResults->at(i).sentence();
+    foreach (const RecognitionResult &result, currentlyPromptedListOfResults) {
+      QString sentence = result.sentence();
       if (sentence == selectedSentence) {
         kDebug() << "Found the result!";
-        RecognitionResultList *list = new RecognitionResultList();
-        list->append(RecognitionResult(currentlyPromptedListOfResults->at(i)));
-        delete currentlyPromptedListOfResults;
-        currentlyPromptedListOfResults = 0;
+        RecognitionResultList list;
+        list.append(result);
+        currentlyPromptedListOfResults.clear();
         processRawResults(list);
         return true;
       }
@@ -121,10 +118,10 @@ bool ActionManager::triggerCommand(const QString& type, const QString& trigger, 
 
 bool ActionManager::processResult(RecognitionResult recognitionResult)
 {
-  if (!greedyReceivers->isEmpty()) {
+  if (!greedyReceivers.isEmpty()) {
     bool accepted = false;
     //		for (int i=0; i < greedyReceivers->count(); i++) {
-    if (greedyReceivers->at(0)->greedyTriggerRaw(recognitionResult))
+    if (greedyReceivers.at(0)->greedyTriggerRaw(recognitionResult))
       accepted = true;
     //			break;
     //		}
@@ -135,64 +132,62 @@ bool ActionManager::processResult(RecognitionResult recognitionResult)
 }
 
 
-void ActionManager::processRawResults(RecognitionResultList* recognitionResults)
+void ActionManager::processRawResults(const RecognitionResultList &recognitionResults)
 {
-  if (recognitionResults->isEmpty())
+  if (recognitionResults.isEmpty())
     return;
 
-  kDebug() << "Processing " << recognitionResults->count() << " raw results";
+  kDebug() << "Processing " << recognitionResults.count() << " raw results";
 
-  RecognitionResultList *selectedRecognitionResults = new RecognitionResultList();
+  RecognitionResultList selectedRecognitionResults;
 
-  if (!currentlyPromptedListOfResults || currentlyPromptedListOfResults->isEmpty()) {
-    for (int i=0; i < recognitionResults->count(); i++) {
+  if (currentlyPromptedListOfResults.isEmpty()) {
+    for (int i=0; i < recognitionResults.count(); i++) {
       //foreach (const RecognitionResult& result, recognitionResults) {
       //if the recognition result has:
       //	* One word that has a score of 0
       //	* An average score of below the minimum confidence
       //it will be not be included in the list of results
 
-      QList<float> confidenceScores = recognitionResults->at(i).confidenceScores();
+      QList<float> confidenceScores = recognitionResults.at(i).confidenceScores();
       kDebug() << confidenceScores;
 
       //calc average
-      float avg= recognitionResults->at(i).averageConfidenceScore();
+      float avg= recognitionResults.at(i).averageConfidenceScore();
       kDebug() << avg << minimumConfidenceThreshold;
 
       if (!confidenceScores.contains(0.0f) && (avg >= minimumConfidenceThreshold))
-        selectedRecognitionResults->append(recognitionResults->at(i));
+        selectedRecognitionResults.append(recognitionResults.at(i));
     }
 
-    kDebug() << "Viable recognition results: " << selectedRecognitionResults->count();
+    kDebug() << "Viable recognition results: " << selectedRecognitionResults.count();
 
-    if (selectedRecognitionResults->count() == 0) {
-      delete selectedRecognitionResults;
+    if (selectedRecognitionResults.isEmpty()) {
       return;
     }
   }
   else {
     //we are already asking...
-    selectedRecognitionResults->append(recognitionResults->at(0));
+    selectedRecognitionResults.append(recognitionResults.at(0));
   }
 
-  kDebug() << "Greedy Recievers: " << greedyReceivers->count();
+  kDebug() << "Greedy Recievers: " << greedyReceivers.count();
 
-  if (!greedyReceivers->isEmpty()) {
+  if (!greedyReceivers.isEmpty()) {
     //		for (int i=0; i < greedyReceivers->count(); i++) {
-    /*if (*/ greedyReceivers->at(0)->greedyTriggerRawList(selectedRecognitionResults); //)
+    /*if (*/ greedyReceivers.at(0)->greedyTriggerRawList(selectedRecognitionResults); //)
     //				break;
     //		}
-    delete selectedRecognitionResults;
+    selectedRecognitionResults.clear();
     return;
   }
 
-  if (selectedRecognitionResults->count() == 1) {
-    processResult(selectedRecognitionResults->at(0));
-  }
-  else {
+  if (selectedRecognitionResults.count() == 1) {
+    processResult(selectedRecognitionResults.at(0));
+  } else {
     presentUserWithResults(selectedRecognitionResults);
   }
-  delete selectedRecognitionResults;
+  selectedRecognitionResults.clear();
 }
 
 
@@ -205,39 +200,34 @@ CommandList ActionManager::getCommandList()
 void ActionManager::resultSelectionDone()
 {
   kDebug() << "resultSelectionDone()";
-  delete currentlyPromptedListOfResults;
-  currentlyPromptedListOfResults = 0;
+  currentlyPromptedListOfResults.clear();
 }
 
 
-void ActionManager::presentUserWithResults(RecognitionResultList* recognitionResults)
+void ActionManager::presentUserWithResults(const RecognitionResultList &recognitionResults)
 {
   kDebug() << "More than one possible recognition result ... should display list!";
-  if (!useDYM || (currentlyPromptedListOfResults &&
-  !currentlyPromptedListOfResults->isEmpty())) {
+  if (!useDYM || !currentlyPromptedListOfResults.isEmpty()) {
     //no double did-you-means...
-    processResult(recognitionResults->at(0));
+    processResult(recognitionResults.at(0));
     return;
   }
-
-  delete currentlyPromptedListOfResults;
-  currentlyPromptedListOfResults = new RecognitionResultList();
-
+  currentlyPromptedListOfResults.clear();
+  
   QStringList sentences;
   QStringList trigger;
   QStringList iconSrcs;
-  for (int i=0; i<recognitionResults->count(); i++) {
+  for (int i=0; i<recognitionResults.count(); i++) {
 
-    QList<float> confidenceScores = recognitionResults->at(i).confidenceScores();
+    QList<float> confidenceScores = recognitionResults.at(i).confidenceScores();
 
-    float avg = recognitionResults->at(i).averageConfidenceScore();
+    float avg = recognitionResults.at(i).averageConfidenceScore();
     avg *= 100;
 
-    sentences << QString("%1%: %2").arg(qRound(avg)/*, 0, 'f', 0*/).arg(recognitionResults->at(i).sentence());
+    sentences << QString("%1%: %2").arg(qRound(avg)/*, 0, 'f', 0*/).arg(recognitionResults.at(i).sentence());
     iconSrcs << "";
     trigger << "simonrecognitionresult";
-    currentlyPromptedListOfResults->append(recognitionResults->at(i));
-
+    currentlyPromptedListOfResults.append(recognitionResults.at(i));
   }
 
   ListCommand *list = new ListCommand(0 /* no manager */, i18n("Did you mean ...?"), "help-hint", i18n("simon is not sure what you meant.\n\nPlease select the correct result from the list below."), sentences, iconSrcs, trigger);
@@ -269,6 +259,4 @@ QHash<CommandListElements::Element, VoiceInterfaceCommand*> ActionManager::getGl
 
 ActionManager::~ActionManager()
 {
-  delete greedyReceivers;
-  delete currentlyPromptedListOfResults;
 }

simon/src/simonappcore/simoncontrol.h
@@ -51,9 +51,9 @@ class ContextManager;
 
 class SIMONAPPCORE_EXPORT SimonControl : public QObject
 {
-  Q_OBJECT
+    Q_OBJECT
 
-    public:
+  public:
     enum SystemStatus
     {
       Disconnected=0,
@@ -103,7 +103,7 @@ class SIMONAPPCORE_EXPORT SimonControl : public QObject
     void disconnectFromServer();
     void connectedToServer();
     void disconnectedFromServer();
-    void wordRecognised(RecognitionResultList* recognitionResults);
+    void wordRecognised(RecognitionResultList recognitionResults);
     void abortConnecting();
 
     void compileModel();

simonlib/simonactions/actionmanager.h
@@ -44,8 +44,8 @@ class SIMONACTIONS_EXPORT ActionManager : public QObject
   private:
     static ActionManager* instance;
 
-    RecognitionResultList *currentlyPromptedListOfResults;
-    QList<GreedyReceiver*> *greedyReceivers;
+    RecognitionResultList currentlyPromptedListOfResults;
+    QList<GreedyReceiver*> greedyReceivers;
 
     float minimumConfidenceThreshold;
     bool useDYM;
@@ -57,8 +57,8 @@ class SIMONACTIONS_EXPORT ActionManager : public QObject
     ActionManager(QObject *parent=0);
 
   public slots:
-    void processRawResults(RecognitionResultList* recognitionResults);
-    void presentUserWithResults(RecognitionResultList* recognitionResults);
+    void processRawResults(const RecognitionResultList &recognitionResults);
+    void presentUserWithResults(const RecognitionResultList &recognitionResults);
     bool processResult(RecognitionResult recognitionResult);
     bool triggerCommand(const QString& type, const QString& trigger, bool silent=false);
 

simonlib/simonactions/greedyreceiver.cpp
@@ -21,18 +21,17 @@
 #include "actionmanager.h"
 #include <simonscenarios/commandmanager.h>
 
-bool GreedyReceiver::greedyTriggerRawList(RecognitionResultList* resultList)
+bool GreedyReceiver::greedyTriggerRawList(const RecognitionResultList &resultList)
 {
   kDebug() << "Greedy triggering raw list";
-  if (resultList->isEmpty()) return false;
+  if (resultList.isEmpty()) return false;
 
-  if (resultList->count() > 1) {
+  if (resultList.count() > 1) {
     kDebug() << "Presenting user with results...";
     ActionManager::getInstance()->presentUserWithResults(resultList);
     return true;
-  }
-  else {
-    return greedyTriggerRaw(resultList->at(0));
+  } else {
+    return greedyTriggerRaw(resultList.at(0));
   }
 }
 

simonlib/simonactions/greedyreceiver.h
@@ -87,7 +87,7 @@ class SIMONACTIONS_EXPORT GreedyReceiver
      * \param results The received list of recognition results
      * \return True, if the command was accepted
      */
-    virtual bool greedyTriggerRawList(RecognitionResultList* results);
+    virtual bool greedyTriggerRawList(const RecognitionResultList &results);
 
     /**
      * \brief Trigger with the raw recognition result

commit 127f40fe925a9eeec38012aec2d3c030316e7c5c

Unbreak recognition.
cd255d2481734665532eea2fd08bf6dda9d07178 broke recognition because it
did miss some QList* that were re-implementing other functions.

plugins/Commands/PronunciationTraining/pronunciationtraining.cpp
@@ -119,9 +119,9 @@ void PronunciationTraining::quit()
 }
 
 
-bool PronunciationTraining::greedyTriggerRawList(RecognitionResultList* results)
+bool PronunciationTraining::greedyTriggerRawList(RecognitionResultList results)
 {
-  if (results->isEmpty()) return false;
+  if (results.isEmpty()) return false;
 
   if (m_currentWordIndex >= m_wordsToTest.count())
     return false;
@@ -130,7 +130,7 @@ bool PronunciationTraining::greedyTriggerRawList(RecognitionResultList* results)
 
   int i=0;
   bool found = false;
-  foreach (const RecognitionResult& result, *results) {
+  foreach (const RecognitionResult& result, results) {
     if (result.sentence() == searchedSentence) {
       kDebug() << "Found result: at index: " << i << result.toString();
       ui.pbScore->setValue(qRound(100.0f * result.averageConfidenceScore()));

plugins/Commands/PronunciationTraining/pronunciationtraining.h
@@ -49,7 +49,7 @@ class PronunciationTraining : public QWidget, public GreedyReceiver
     void closeEvent(QCloseEvent *);
 
   public:
-    bool greedyTriggerRawList(RecognitionResultList* results);
+    bool greedyTriggerRawList(RecognitionResultList results);
     void init();
     explicit PronunciationTraining(const QString& terminal, QWidget* parent=0);
 

simon/src/simonappcore/simoncontrol.cpp
@@ -65,7 +65,7 @@ SimonControl::SimonControl(QWidget *parent) : QObject (parent)
 
   QObject::connect(RecognitionControl::getInstance(), SIGNAL(loggedIn()), this, SLOT(loggedIn()));
 
-  QObject::connect(RecognitionControl::getInstance(), SIGNAL(recognised(RecognitionResultList*)), this, SLOT(wordRecognised(RecognitionResultList*)));
+  QObject::connect(RecognitionControl::getInstance(), SIGNAL(recognised(RecognitionResultList)), this, SLOT(wordRecognised(RecognitionResultList)));
   QObject::connect(RecognitionControl::getInstance(), SIGNAL(recognitionStatusChanged(RecognitionControl::RecognitionStatus)), this, SLOT(recognitionStatusChanged(RecognitionControl::RecognitionStatus)));
 
   QObject::connect(ScenarioManager::getInstance(), SIGNAL(deactivatedScenarioListChanged()), this, SIGNAL(deactivatedScenarioListChanged()));

simon/src/simonrecognitioncontrol/recognitioncontrol.cpp
@@ -1750,7 +1750,7 @@ void RecognitionControl::messageReceived()
           msg >> sentenceCount;
 
 	  emit receivedResults();
-          RecognitionResultList* recognitionResults = new RecognitionResultList();
+          RecognitionResultList recognitionResults;
 
           for (int i=0; i < sentenceCount; i++) {
             QByteArray word, sampa, samparaw;
@@ -1759,7 +1759,7 @@ void RecognitionControl::messageReceived()
             msg >> sampa;
             msg >> samparaw;
             msg >> confidenceScores;
-            recognitionResults->append(RecognitionResult(QString::fromUtf8(word),
+            recognitionResults.append(RecognitionResult(QString::fromUtf8(word),
               QString::fromUtf8(sampa),
               QString::fromUtf8(samparaw),
               confidenceScores));

simon/src/simonrecognitioncontrol/recognitioncontrol.h
@@ -124,7 +124,7 @@ class RECOGNITIONCONTROL_EXPORT RecognitionControl : public SimonSender
     void loggedIn();
 
     void recognitionStatusChanged(RecognitionControl::RecognitionStatus);
-    void recognised(RecognitionResultList* recognitionResults);
+    void recognised(RecognitionResultList recognitionResults);
 
     void modelsAvailable(const QList<QDateTime>& models);
 

simonlib/simonactions/actionmanager.cpp
@@ -94,7 +94,7 @@ void ActionManager::retrieveRecognitionResultFilteringParameters()
 
 bool ActionManager::triggerCommand(const QString& type, const QString& trigger, bool silent)
 {
-  if (type == "simonrecognitionresult") {
+  if (type == "simonrecognitionresult" && currentlyPromptedListOfResults.count()) {
     //result from a did-you-mean popup
     QString selectedSentence = trigger;
     selectedSentence.remove(QRegExp("^[0-9][0-9]?[0-9]?%: "));