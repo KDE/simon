0393465ea2c93e97aba3868495645dd007642a97
Peter Grasch <grasch@simon-listens.org>
2012-02-08 10:31:07 +0100

    Removing confidence scores for sentence markers

--- simonlib/simonrecognizer/juliusrecognizer.cpp
+++ simonlib/simonrecognizer/juliusrecognizer.cpp
@@ -163,6 +163,10 @@ QList< RecognitionResult > JuliusRecognizer::recognize(const QString& file)
     QString confStr = r.mid(r.indexOf(cmScore));
     confStr = confStr.left(confStr.indexOf('\n'));
     confStr.remove(cmScore);
+    if (confidenceScores.size() > 1) { // otherwise we have no sentence marker
+      confidenceScores.takeFirst();
+      confidenceScores.takeLast(); // remove confidence scores for <s> and </s>
+    }
     bool ok = true;
     foreach (const QString& scoreStr, confStr.split(' ', QString::SkipEmptyParts)) {
       confidenceScores.append(scoreStr.toFloat(&ok) / 100.0f);
