0a79d74113ecb20be7c8b1571c58c82a9ae69952
Peter Grasch <grasch@simon-listens.org>
2012-02-08 09:49:21 +0100

    Updating build scripts since Julius is no longer shipped with simon

--- build_ubuntu.sh
+++ build_ubuntu.sh
@@ -3,5 +3,5 @@
 # This script will build and install simon
 
 mkdir build 2> /dev/null
-cd build && cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` .. && make ; touch ./julius/gramtools/mkdfa/mkfa-1.44-flex/* && make && sudo make install && sudo ldconfig && kbuildsycoca4 && echo -e "**** Build completed ****\n\nThe executable file \"simon\" is now ready and has been installed.\n\nIssue \"simon\" to start it."
+cd build && cmake -DCMAKE_INSTALL_PREFIX=`kde4-config --prefix` .. && make && sudo make install && sudo ldconfig && kbuildsycoca4 && echo -e "**** Build completed ****\n\nThe executable file \"simon\" is now ready and has been installed.\n\nIssue \"simon\" to start it."
 
